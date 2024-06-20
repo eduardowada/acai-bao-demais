0ad13969f8d54e0bc8850fb1926994867b291532
Eduardo Wada <156962925+eduardowada@users.noreply.github.com>
2024-06-20 15:40:50 -0300

    Envio do Controller e do CRUD

--- projeto_final_bloco_01/src/ecommerce/Menu.java
+++ projeto_final_bloco_01/src/ecommerce/Menu.java
@@ -1,79 +1,180 @@
 package ecommerce;
 
-import java.util.*;
-
+import ecommerce.controller.ProdutoController;
+import ecommerce.model.Acai;
+import ecommerce.model.AcaiBranco;
+import ecommerce.model.AcaiPuro;
+import ecommerce.model.AcaiTrufado;
+import ecommerce.model.Produto;
 import ecommerce.util.Cores;
 
-public class Menu {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int opcao;
-		
-		while (true) {
-			
-			System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND
-					+ "*****************************************************");
-			System.out.println("                                                     ");
-			System.out.println("                  AÇAÍ BÃO DEMAIS!		     ");
-			System.out.println("                                                     ");
-			System.out.println("*****************************************************");
-			System.out.println("                                                     ");
-			System.out.println("            1 - Cadastrar Produto                    ");
-			System.out.println("            2 - Listar Produtos               	     ");
-			System.out.println("            3 - Buscar Produto por Numero            ");
-			System.out.println("            4 - Atualizar Produto                    ");
-			System.out.println("            5 - Deletar Produto                      ");
-			System.out.println("            6 - Sair                                 ");
-			System.out.println("                                                     ");
-			System.out.println("*****************************************************");
-			System.out.println("Entre com a opção desejada:                          ");
-			System.out.println("                                                     " + Cores.TEXT_RESET);
-			
-			opcao = sc.nextInt();
-			
-			if (opcao == 6) {
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nAçaí bão demais - NOSSA! BÃO DEMAIS SÔ");
-				sobre();
-				sc.close();
-				System.exit(0);
-			}
-			
-			switch (opcao) {
-			case 1:
-				System.out.println("Cadastrar Produto \n\n");
-				
-				break;
-			case 2:
-				System.out.println("Listar Produtos \n\n");
-				
-				break;
-			case 3:
-				System.out.println("Buscar Produto por Numero \n\n");
-				
-				break;
-			case 4:
-				System.out.println("Atualizar Produto \n\n");
-				
-				break;
-			case 5:
-				System.out.println("Deletar Produto\n\n");
-				
-				break;
-			default:
-				System.out.println("\nOpção Inválida!\n");
-				break;
-			}
-		}
-	}
-	
-	public static void sobre() {
-		System.out.println("\n*********************************************************");
-		System.out.println("Projeto Desenvolvido por: Eduardo Wada");
-		System.out.println("https://github.com/eduardowada/acai-bao-demais.git");
-		System.out.println("Projetro para o Bootcamp da Generation Brasil");
-		System.out.println("Projeto desenvolvido para avaliação final do módulo 1.");
-		System.out.println("*********************************************************");
-	}
+public class Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProdutoController controller = new ProdutoController();
+        int opcao;
+
+        /*
+         Exemplo de Criação 1:
+         ID: 1
+         Nome: Copo da Felicidade
+         Preço: 15 
+         Tamanho: Grande
+         Tipo: 3*/
+        
+        /*
+        Exemplo de Criação 1:
+        ID: 2
+        Nome: Copo Espanta Tristeza
+        Preço: 20
+        Tamanho: Médio
+        Tipo: 2*/
+        
+        while (true) {
+            System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND
+                    + "*****************************************************");
+            System.out.println("                                                     ");
+            System.out.println("      CADASTRO DE E-COMMERCE: AÇAÍ BÃO DEMAIS!       ");
+            System.out.println("                                                     ");
+            System.out.println("*****************************************************");
+            System.out.println("                                                     ");
+            System.out.println("            1 - Cadastrar Produto                    ");
+            System.out.println("            2 - Listar Produtos                      ");
+            System.out.println("            3 - Buscar Produto por Numero            ");
+            System.out.println("            4 - Atualizar Produto                    ");
+            System.out.println("            5 - Deletar Produto                      ");
+            System.out.println("            6 - Sair                                 ");
+            System.out.println("                                                     ");
+            System.out.println("*****************************************************");
+            System.out.println("Entre com a opção desejada:                          ");
+            System.out.println("                                                     " + Cores.TEXT_RESET);
+
+            opcao = sc.nextInt();
+
+            if (opcao == 6) {
+                System.out.println(Cores.TEXT_WHITE_BOLD + "\nAçaí bão demais - NOSSA! BÃO DEMAIS SÔ");
+                sobre();
+                sc.close();
+                System.exit(0);
+            }
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastrar Produto \n\n");
+                    cadastrarProduto(controller, sc);
+                    break;
+                case 2:
+                    System.out.println("Listar Produtos \n\n");
+                    listarProdutos(controller);
+                    break;
+                case 3:
+                    System.out.println("Buscar Produto por Numero \n\n");
+                    buscarProduto(controller, sc);
+                    break;
+                case 4:
+                    System.out.println("Atualizar Produto \n\n");
+                    atualizarProduto(controller, sc);
+                    break;
+                case 5:
+                    System.out.println("Deletar Produto\n\n");
+                    deletarProduto(controller, sc);
+                    break;
+                default:
+                    System.out.println("\nOpção Inválida!\n");
+                    break;
+            }
+        }
+    }
+
+    private static void cadastrarProduto(ProdutoController controller, Scanner sc) {
+        System.out.println("Digite o ID do produto: ");
+        int id = sc.nextInt();
+        sc.nextLine(); // Consumir a nova linha
+        System.out.println("Digite o nome do produto: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite o preço do produto: ");
+        double preco = sc.nextDouble();
+        sc.nextLine(); // Consumir a nova linha
+        System.out.println("Digite o tamanho do produto: ");
+        String tamanho = sc.nextLine();
+        System.out.println("Escolha o tipo de açaí:");
+        System.out.println("1 - Açaí Puro");
+        System.out.println("2 - Açaí Trufado");
+        System.out.println("3 - Açaí Branco");
+        int tipo = sc.nextInt();
+
+        Produto produto;
+        switch (tipo) {
+            case 1:
+                produto = new AcaiPuro(id, nome, preco, tamanho);
+                break;
+            case 2:
+                produto = new AcaiTrufado(id, nome, preco, tamanho);
+                break;
+            case 3:
+                produto = new AcaiBranco(id, nome, preco, tamanho);
+                break;
+            default:
+                System.out.println("Tipo de produto inválido!");
+                return;
+        }
+
+        controller.adicionarProduto(produto);
+    }
+
+    private static void listarProdutos(ProdutoController controller) {
+        for (Produto produto : controller.listarProdutos()) {
+            System.out.println(produto);
+        }
+    }
+
+    private static void buscarProduto(ProdutoController controller, Scanner sc) {
+        System.out.println("Digite o ID do produto: ");
+        int id = sc.nextInt();
+        Produto produto = controller.buscarProdutoPorId(id);
+        if (produto != null) {
+            System.out.println(produto);
+        } else {
+            System.out.println("Produto não encontrado!");
+        }
+    }
+
+    private static void atualizarProduto(ProdutoController controller, Scanner sc) {
+        System.out.println("Digite o ID do produto a ser atualizado: ");
+        int id = sc.nextInt();
+        Produto produto = controller.buscarProdutoPorId(id);
+        if (produto != null) {
+            sc.nextLine(); // Consumir a nova linha
+            System.out.println("Digite o novo nome do produto: ");
+            String nome = sc.nextLine();
+            System.out.println("Digite o novo preço do produto: ");
+            double preco = sc.nextDouble();
+            sc.nextLine(); // Consumir a nova linha
+            System.out.println("Digite o novo tamanho do produto: ");
+            String tamanho = sc.nextLine();
+            produto.setNome(nome);
+            produto.setPreco(preco);
+            ((Acai) produto).setTamanho(tamanho); // Typecast para Acai
+            controller.atualizarProduto(produto);
+        } else {
+            System.out.println("Produto não encontrado!");
+        }
+    }
+
+    private static void deletarProduto(ProdutoController controller, Scanner sc) {
+        System.out.println("Digite o ID do produto a ser deletado: ");
+        int id = sc.nextInt();
+        controller.deletarProduto(id);
+    }
+
+    public static void sobre() {
+        System.out.println("\n*********************************************************");
+        System.out.println("Projeto Desenvolvido por: Eduardo Wada");
+        System.out.println("https://github.com/eduardowada/acai-bao-demais.git");
+        System.out.println("Projeto para o Bootcamp da Generation Brasil");
+        System.out.println("Projeto desenvolvido para avaliação final do módulo 1.");
+        System.out.println("*********************************************************");
+    }
 }
